5db9382c17aea9d604373cd1eb916b4e441e7f93
MEG HOWE <howem9@gmail.com>
2021-07-06 14:52:23 -0400

    Filled in join table using console

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,18 +6,21 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-post = Post.new(title: "So Funny", text: "What's the deal with airline food?")
-post = Post.new(title: "Haha", text: "Tell me a funny joke.")
+post = Post.create!(title: "So Funny", text: "What's the deal with airline food?")
+post = Post.create!(title: "Haha", text: "Tell me a funny joke.")
 
-post = Post.new(title: "How many continents?", text: "Casey's been to all the continents!")
-post = Post.new(title: "Where?", text: "Where should I go next?")
-post = Post.new(title: "How?", text: "I could live in Hawaii if I worked on an avocado farm. Dat good fat.")
+post = Post.create!(title: "How many continents?", text: "Casey's been to all the continents!")
+post = Post.create!(title: "Where?", text: "Where should I go next?")
+post = Post.create!(title: "How?", text: "I could live in Hawaii if I worked on an avocado farm. Dat good fat.")
 
-post = Post.new(title: "Tell Me", text: "What is the meaning of life")
-post = Post.new(title: "We Croak?", text: "Check out my dope bucket list.")
+post = Post.create!(title: "Tell Me", text: "What is the meaning of life")
+# post = Post.create!(title: "We Croak?", text: "Check out my dope bucket list.")
 
-tag = Tag.new(name: "Comedy")
-tag = Tag.new(name: "Travel")
-tag = Tag.new(name: "Life")
+# post.save
 
+tag = Tag.create!(name: "Comedy")
+tag = Tag.create!(name: "Travel")
+# tag = Tag.new(name: "Life")
+
+# tag.save
 #post_tag = Post_Tag.new(post_id: 1, tag_id: 1)
